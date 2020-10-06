93d34a8ea4931a4c06ac245be02abf248d894b09
Andrey P <fouks04@mail.ru>
2020-10-06 11:47:06 +0300

    add includes for N+1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @user = current_user
     # @user.role == "admin" ? render "show_admin" : render "show"
     if @user.role == "user"
+      @user = User.includes(tasks: [:lists, :subtasks], lists: [:tasks]).find(current_user[:id])
       render "index"
     elsif @user.role == "admin"
       render "index_admin"
