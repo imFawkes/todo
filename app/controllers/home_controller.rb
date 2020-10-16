7ffc8d3a931637a1624c33b959ec239988b63e97
Andrey P <fouks04@mail.ru>
2020-10-16 18:09:40 +0300

    change instance var to local var in homecontroller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,24 +2,13 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @user = current_user
-    # @user.role == "admin" ? render "index_admin" : render "index" # don't working
-    if @user.user?
-      @user = User.includes(tasks: [:lists, :subtasks], lists: [:tasks]).find(current_user[:id])
-      render 'index', locals: { user: @user }
-    elsif @user.admin?
-      render 'index_admin', locals: { user: @user }
+    user = current_user
+    if user.user?
+      user = User.includes(tasks: [:lists, :subtasks], lists: [:tasks]).find(current_user[:id])
+      render 'index', locals: { user: user }
+    elsif user.admin?
+      render 'index_admin', locals: { user: user }
     end
   end
 
-  # def index
-  #   @user = current_user
-  #   # @user.role == "admin" ? render "index_admin" : render "index" # don't working
-  #   if @user.role.downcase == 'user'
-  #     @user = User.includes(tasks: [:lists, :subtasks], lists: [:tasks]).find(current_user[:id])
-  #     render 'index', locals: { user: @user }
-  #   elsif @user.role.downcase == 'admin'
-  #     render 'index_admin', locals: { user: @user }
-  #   end
-  # end
 end
