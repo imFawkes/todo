b1b07b3c637156df5a73289c5a039bc078ac9b6d
Andrey P <fouks04@mail.ru>
2020-10-19 19:20:40 +0300

    fix list creation

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,19 @@
 class ListsController < ApplicationController
   def create
+    @user = User.find(params[:user_id])
+    list = @user.lists.create(list_params)
+    redirect_to root_path
+
     # ! переделать
-    creator = Lists::Create.new
+    # creator = Lists::Create.new
 
-    result = creator.call(params)
+    # result = creator.call(params)
 
-    if result.success?
-      render json: { object: result.object.to_json, code: 200 }
-    else
-      render json: { errors: result.errors.messages, code: 422 }
-    end
+    # if result.success?
+    #   render json: { object: result.object.to_json, code: 200 }
+    # else
+    #   render json: { errors: result.errors.messages, code: 422 }
+    # end
   end
 
   def destroy
@@ -17,5 +21,11 @@ class ListsController < ApplicationController
     list = @user.lists.find(params[:id])
     list.destroy
     redirect_to root_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
